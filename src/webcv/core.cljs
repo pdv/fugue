6dfc1b67874bf9f38b0a251cb8c702f017c00d17
pdv <philipjdelvecchio@gmail.com>
2020-04-25 13:33:51 -0400

    Add reagent

--- src/webcv/core.cljs
+++ src/webcv/core.cljs
@@ -1,21 +1,11 @@
 (ns webcv.core
-    (:require ))
+  (:require [reagent.core :as r]
+            [reagent.dom :as rdom]))
 
-(enable-console-print!)
-
-(defonce app-state (atom {:text "Hello world!"}))
-
-(defn on-js-reload []
-  ;; optionally touch your app-state to force rerendering depending on
-  ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
-)
-
-(def ctx (js/AudioContext.))
-(def dest (.-destination ctx))
-(set! (.-channelCount dest) (.-maxChannelCount dest))
-(def mixer (.createChannelMerger ctx (.-channelCount dest)))
-(.connect mixer dest)
+(defonce ctx (js/AudioContext.))
+(defonce dest (.-destination ctx))
+(defonce out-channel-count (.-maxChannelCount dest))
+(defonce mixer (.createChannelMerger ctx out-channel-count))
 
 (defn out [n v]
   (.connect v mixer 0 n))
@@ -25,3 +15,15 @@
     (set! (.-value (.-frequency node)) freq)
     (.start node)
     node))
+
+(defn greeting [message]
+  [:h1 message])
+
+(defn main []
+  (enable-console-print!)
+  (set! (.-channelCount dest) out-channel-count)
+  (.connect mixer dest)
+  (rdom/render [greeting "howdy"] (js/document.getElementById "app")))
+
+(main)
+
