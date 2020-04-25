0d4d4f45ea9329d1d7076f77feccaa41fb072750
pdv <philipjdelvecchio@gmail.com>
2020-04-25 14:51:23 -0400

    Add ins

--- src/webcv/core.cljs
+++ src/webcv/core.cljs
@@ -2,26 +2,45 @@
   (:require [reagent.core :as r]
             [reagent.dom :as rdom]))
 
-(defonce state (atom {:ctx nil :mixer nil}))
+(defonce state (atom {:ctx nil :outs nil :ins nil}))
 
-(defn reset-ctx! []
-  (when-let [old-ctx (:ctx @state)]
-    (.close old-ctx))
-  (let [ctx (js/AudioContext.)
-        dest (.-destination ctx)
-        chans (.-maxChannelCount dest)
-        mixer (.createChannelMerger ctx chans)]
-    (set! (.-channelCount dest) chans)
-    (.connect mixer dest)
-    (reset! state {:ctx ctx :mixer mixer})))
+(defn reset-ctx! 
+  ([]
+   (when-let [old-ctx (:ctx @state)]
+     (.close old-ctx))
+   (.then (.getUserMedia (.-mediaDevices js/navigator)
+                         (clj->js {:audio true}))
+          (partial reset-ctx!)))
+  ([in-stream]
+   (let [ctx (js/AudioContext.)
+         dest (.-destination ctx)
+         out-chans (.-maxChannelCount dest)
+         outs (.createChannelMerger ctx out-chans)
+         source (.createMediaStreamSource ctx in-stream)
+         in-chans (.-channelCount source)
+         ins (.createChannelSplitter ctx in-chans)]
+     (set! (.-channelCount dest) out-chans)
+     (.connect outs dest)
+     (.connect source ins)
+     (reset! state {:ctx ctx :outs outs :ins ins}))))
 
 (defn out [n v]
-  (.connect v (:mixer @state) 0 (dec n)))
+  (.connect v (:outs @state) 0 (dec n)))
+
+(defn in [n]
+  (let [node (.createGain (:ctx @state))]
+    (.connect (:ins @state) node (dec n))
+    node))
 
 (defn sin-osc [freq]
-  (let [ctx (:ctx @state)
-        node (.createOscillator ctx)]
+  (let [node (.createOscillator (:ctx @state))]
     (set! (.-value (.-frequency node)) freq)
+    (.start node)
+    node))
+
+(defn const [v]
+  (let [node (.createConstantSource (:ctx @state))]
+    (set! (.-value (.-offset node)) v)
     (.start node)
     node))
 
