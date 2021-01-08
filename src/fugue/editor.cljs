80ec5e1507a8a010345917ae5bda139b495f52ac
pdv <philipjdelvecchio@gmail.com>
2021-01-08 13:45:01 -0500

    Editor settings

--- src/fugue/editor.cljs
+++ src/fugue/editor.cljs
@@ -4,17 +4,23 @@
             [cljs.pprint :refer [pprint]]
             [cljs.repl :refer [Error->map]]))
 
-(defn editor [init on-change]
+(defn editor [init on-change settings]
+  (let [codemirror (volatile! nil)]
   (r/create-class
     {:render
      (fn [] [:textarea.editor {:default-value init}])
      :component-did-mount
      (fn [this]
        (let [node (rdom/dom-node this)
-             settings #js {:mode "clojure" :lineNumbers true}
+             settings (clj->js (merge settings {:mode "clojure" :lineNumbers true}))
              cm (.fromTextArea js/CodeMirror node settings)]
          (.on cm "change" #(on-change (.getValue %)))
-         (js/setTimeout #(on-change init) 5)))}))
+         (vreset! codemirror cm)
+         (js/setTimeout #(on-change init) 5)))
+     :component-did-update
+     (fn [this old-argv]
+       (doseq [[key value] (last (r/argv this))]
+         (.setOption @codemirror (clj->js key) value)))})))
 
 (defn top-text [text]
   [:textarea.repl-out {:read-only true :value text}])
--- src/fugue/ide.cljs
+++ src/fugue/ide.cljs
@@ -4,16 +4,20 @@
 
 (defn ide [init eval-fn]
   (let [input (r/atom init)
-        output (r/atom nil)]
+        output (r/atom nil)
+        vim-on (r/atom true)]
     (fn []
       [:div.ide
        [:div.ide-left
         (when @output
           [output-box @output])]
        [:div.ide-right
-        [editor init (partial reset! input)]
+        [editor init (partial reset! input) {:keyMap (if @vim-on "vim" "default")}]
         [:div.ide-toolbar
          [:button#eval
           {:on-click #(eval-fn @input (partial reset! output))}
-          "eval"]]
-        [editor "repl" (partial print)]]])))
+          "eval"]
+         [:button
+          {:on-click #(swap! vim-on not)}
+          (str "vim on?" @vim-on)]]
+        [editor "repl" (partial print) {:readOnly true}]]])))
