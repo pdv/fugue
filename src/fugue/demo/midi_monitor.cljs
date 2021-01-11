02bba94eff3b74a57714bc88bd48e476bbf22904
pdv <philipjdelvecchio@gmail.com>
2021-01-11 12:28:23 -0500

    Update chord detector demo again

--- src/fugue/demo/midi_monitor.cljs
+++ src/fugue/demo/midi_monitor.cljs
@@ -19,26 +19,20 @@
 
 (def note-names [:C :Db :D :Eb :E :F :F# :G :Ab :A :Bb :B])
 
+(defn chords-from [predicate root notes]
+  (let [relative-notes (map #(mod (- % root) 12) notes)]
+    (->> chord-names
+         (filter (fn [[_ chord]] (predicate chord relative-notes)))
+         (map (fn [[name _]] [(nth note-names (mod root 12)) name])))))
+
 (defn chords-where [predicate notes]
-  (into #{}
-        (mapcat (fn [root]
-                  (let [relative (map #(mod (- % root) 12) notes)]
-                    (->> chord-names
-                         (filter (fn [[_ chord]]
-                                   (predicate chord relative)))
-                         (map (fn [[name _]]
-                                [(nth note-names (mod root 12)) name])))))
-                notes)))
+  (into #{} (mapcat #(chords-from predicate % notes) notes)))
 
-(defn possible-chords [notes]
-  (chords-where (fn [chord relative]
-                  (every? #(contains? chord %) relative))
-                notes))
+(defn every-note-in-chord? [chord relative-notes]
+  (every? #(contains? chord %) relative-notes))
 
-(defn contained-chords [notes]
-  (chords-where (fn [chord relative]
-                  (every? #(contains? (set relative) %) chord))
-                notes))
+(defn chord-contained-in-notes? [chord relative-notes]
+  (every? #(contains? (set relative-notes) %) chord))
 
 (defn component []
   (let [midi-in-chan (async/chan)
@@ -53,7 +47,7 @@
       [:div
        [midi-input midi-in-chan]
        [:p @held-notes]
-       (for [chord (possible-chords @held-notes)]
+       (for [chord (chords-where every-note-in-chord? @held-notes)]
          [:p (str chord)])])))
 
 [component]
