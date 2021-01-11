b5e7a8ded901d6d9ebb5c5d329284d79dcbec2eb
pdv <philipjdelvecchio@gmail.com>
2021-01-10 22:09:56 -0500

    Add cof demo

--- src/fugue/api.cljs
+++ src/fugue/api.cljs
@@ -52,10 +52,12 @@
 (def sequencer sequencer/sequencer)
 (def sampler sampler/sampler)
 (def conv convolver/convolver)
-(def monitor monitor/monitor)
+
+(def make-midi-ctx midi/make-ctx)
+(def midi-context-manager ctrls/midi-ctx-ctrls)
+(def midi-input-monitor monitor/input-monitor)
 
 (def audio-ctx-ctrls ctrls/audio-ctx-ctrls)
-(def midi-ctx-ctrls ctrls/midi-ctx-ctrls)
 (def buffer-ctrl ctrls/buffer-ctrl)
 (def slider components/slider)
 (def picker components/picker)
@@ -125,6 +127,33 @@
         \"Colors\"
         [picker colors (keys color-fns)]
         [cantor @root @harmonics (get color-fns @colors)]])))
+
+[component]
+")
+
+(def midi-monitor-demo "
+(def names
+  [[\"C\" 0] [\"G\" 7] [\"D\" 2] [\"A\" 9] [\"E\" 4] [\"B\" 11]
+   [\"F#\" 6] [\"Db\" 1] [\"Ab\" 8] [\"Eb\" 3] [\"Bb\" 10] [\"F\" 5]])
+
+(defn cof [midi-notes]
+  (let [intervals (into #{} (map #(mod % 12) midi-notes))]
+    [:div.cof-container>div.cof>ul
+    (for [[name interval] names]
+      [:li>i
+       {:class (if (contains? intervals interval) \"active\" \"inactive\")}
+       name])]))
+
+(defn component []
+  (let [midi-ctx (ratom nil)
+        notes (ratom #{})]
+    (fn []
+      [:div
+        (if-let [mctx @midi-ctx]
+          [midi-input-monitor @midi-ctx (partial reset! notes)]
+          [:button {:on-click #(midi/make-ctx (partial reset! midi-ctx))}
+            (str (if @midi-ctx \"reset\" \"create\") \" midi context\")])
+        [cof @notes]])))
 
 [component]
 ")
--- src/fugue/midi_monitor.cljs
+++ src/fugue/midi_monitor.cljs
@@ -5,18 +5,9 @@
             [cljs.core.async :as async]
             [fugue.cof :refer [cof]]
             [fugue.cantor :as cantor]
-            [fugue.components :refer [slider]]
+            [fugue.components :refer [slider picker]]
             [fugue.midi :as midi]
             [fugue.chords :as chords]))
-
-(defn midi-selector [midi-ctx on-change]
-  (if-let [mctx @midi-ctx]
-    (do
-      [:select {:on-change #(on-change ((::midi/ins mctx) (.. % -target -value)))}
-       [:option {:value nil} "Select an input"]
-       (for [[in-name] (::midi/ins mctx)]
-         [:option {:value in-name} in-name])])
-    [:p "no midi ctx"]))
 
 (defn note-on? [midi-msg]
   (and (= ::midi/note-on (::midi/type midi-msg))
@@ -28,58 +19,30 @@
     ::midi/note-on (= 0 (::midi/velocity midi-msg))
     false))
 
-(defn ratios [notes]
-  (let [freqs (map midi/note->hz (sort notes))]
-    (for [x freqs y freqs]
-      (/ x y))))
+(defn midi-selector [midi-ins on-change]
+  [:select {:on-change #(on-change (get midi-ins (.. % -target -value)))}
+   [:option {:value nil} "Select an input"]
+   (for [[in-name _] midi-ins]
+     [:option {:value in-name} in-name])])
 
-(defn other-thing [notes]
-  (let [freqs (map midi/note->hz (sort notes))]
-    (for [x freqs y freqs]
-      (- y x))))
-
-(defn note-monitor-view [notes]
-  [:div
-   [cof notes]
-   [cantor/cantor-table (or (first notes) 69) 14]
-   [:p (str (sort (into #{} notes)))]
-   [:p (str (sort (into #{} (map #(mod % 12) notes))))]
-   [:p (str (sort (map (comp int midi/note->hz) notes)))]
-   [:ul
-    (for [ratio (into #{} (ratios notes))]
-      [:li (format "%.2f" ratio)])]
-   [:ul
-    (for [ratio (into #{} (other-thing notes))]
-      [:li (format "%.2f" ratio)])]
-   [:hr]
-   [:ul
-    (for [chord (chords/possible-chords notes)]
-      [:li (str chord)])]
-   [:hr]
-   [:ol
-    (for [chord (chords/contained-chords notes)]
-      [:li (str chord)])]])
-
-(defn monitor [midi-ctx]
+(defn input-monitor [_ notes-cb]
   (let [msgs (r/atom {})
         in-mult (r/atom nil)
         in-chan (async/chan)]
     (async/go-loop []
-                   (let [msg (async/<! in-chan)]
-                     (print msg)
-                     (cond
-                       (note-on? msg)
-                       (swap! msgs assoc (::midi/note msg) msg)
-                       (note-off? msg)
-                       (swap! msgs dissoc (::midi/note msg)))
-                     (recur)))
-    (fn []
-      [:div
-       [midi-selector midi-ctx (fn [in]
-                                 (when-let [old-mult @in-mult]
-                                   (async/untap old-mult in-chan))
-                                 (reset! msgs {})
-                                 (when-let [new-mult in]
-                                   (async/tap new-mult in-chan))
-                                 (reset! in-mult in))]
-       [note-monitor-view (keys @msgs)]])))
+      (let [msg (async/<! in-chan)]
+        (cond
+          (note-on? msg) (swap! msgs assoc (::midi/note msg) msg)
+          (note-off? msg) (swap! msgs dissoc (::midi/note msg)))
+        (notes-cb (keys @msgs))
+        (recur)))
+    (fn [midi-ctx]
+      [midi-selector
+       (::midi/ins midi-ctx)
+       (fn [in]
+         (when-let [old-mult @in-mult]
+           (async/untap old-mult in-chan))
+         (reset! msgs {})
+         (when-let [new-mult in]
+           (async/tap new-mult in-chan))
+         (reset! in-mult in))])))
